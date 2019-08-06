e7217ede63003c089173dd104493dae625aecbf0
5h5ong <Call5h5ong@gmail.com>
2019-08-06 19:19:39 +0900

    #5.2 Footer and Auth IU part Three

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Input from '../Components/Input';
+import Button from '../Components/Button';
 
 const Wrapper = styled.div`
   min-height: 80vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 `;
 
 const Box = styled.div`
   ${props => props.theme.whiteBox}
   border-radius: 0px;
   width: 350px;
+  max-width: 100%;
 `;
 
 const StateChanger = styled(Box)`
@@ -23,11 +27,44 @@ const Link = styled.span`
   cursor: pointer;
 `;
 
+const Form = styled(Box)`
+  padding: 40px;
+  padding-bottom: 30px;
+  margin-bottom: 15px;
+  form {
+    width: 100;
+  }
+  input {
+    width: 100%;
+    &:not(:last-child) {
+      margin-bottom: 7px;
+    }
+  }
+`;
+
 export default () => {
   const [action, setAction] = useState('logIn');
 
   return (
     <Wrapper>
+      <Form>
+        {action === 'logIn' ? (
+          <form>
+            <Input placeholder={'login'} />
+            <Input placeholder={'password'} />
+            <Button text={'Log In'} />
+          </form>
+        ) : (
+          <form>
+            <Input placeholder={'First name'} />
+            <Input placeholder={'Last name'} />
+            <Input placeholder={'Email'} />
+            <Input placeholder={'Username'} />
+            <Input placeholder={'Password'} />
+            <Button text={'Sign Up'} />
+          </form>
+        )}
+      </Form>
       <StateChanger>
         {action === 'logIn' ? (
           <>
