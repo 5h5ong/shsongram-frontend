76da095587d832ec780ccee89a9d189cdc812929
5h5ong <Call5h5ong@gmail.com>
2019-08-14 22:32:26 +0900

    #8.2 Search Screen UI Part Two

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -2,17 +2,63 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import FatText from '../../Components/FatText';
+import Loader from '../../Components/Loader';
+import UserCard from '../../Components/UserCard';
 
 const Wrapper = styled.div`
   height: 50vh;
-  text-align: center;
 `;
-const SearchPresenter = ({ searchTerm, loading }) => (
-  <Wrapper>
-    {searchTerm === '' && <FatText text={'무엇이든 찾아보세요!'} />}
-  </Wrapper>
-);
+const Section = styled.div`
+  margin-bottom: 50px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  grid-template-rows: 160px;
+  grid-auto-rows: 160px;
+`;
 
+const SearchPresenter = ({ searchTerm, loading, data }) => {
+  if (searchTerm === '') {
+    return (
+      <Wrapper>
+        {searchTerm === '' && <FatText text={'무엇이든 찾아보세요!'} />}
+      </Wrapper>
+    );
+  } else if (loading === true) {
+    return (
+      <Wrapper>
+        <Loader />
+      </Wrapper>
+    );
+  } else if (data && data.searchUser && data.searchPost) {
+    return (
+      <Wrapper>
+        <Section>
+          {data.searchUser.length === 0 ? (
+            <FatText text={'유저를 찾지 못했습니다.'} />
+          ) : (
+            data.searchUser.map(user => (
+              <UserCard
+                key={user.id}
+                username={user.username}
+                isFollowing={user.isFollowing}
+                url={user.avatar}
+                isSelf={user.isSelf}
+              />
+            ))
+          )}
+        </Section>
+        <Section>
+          {data.searchPost.length === 0 ? (
+            <FatText text={'포스트를 찾지 못했습니다.'} />
+          ) : (
+            data.searchPost.map(post => null)
+          )}
+        </Section>
+      </Wrapper>
+    );
+  }
+};
 SearchPresenter.propTypes = {
   searchTerm: PropTypes.string,
   loading: PropTypes.bool
